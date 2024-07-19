ede2880717122db6b77ce15dd9ae8bcc4719fd9f
Angelica Gallo <angaar97@gmail.com>
2024-07-18 20:03:31 -0500

    fix max height

--- src/presentation/layouts/DashboardLayout.tsx
+++ src/presentation/layouts/DashboardLayout.tsx
@@ -5,7 +5,7 @@ import { SidebarItem } from "../components";
 export const DashboardLayout = () => {
   return (
     <main className="p-1">
-      <div className="flex flex-row w-11/12 mx-auto bg-white bg-opacity-10 rounded-s-xl	mt-3 h-1/5	">
+      <div className="flex flex-row w-11/12 mx-auto bg-white bg-opacity-10 rounded-s-xl	mt-3 h-auto	 max-h-screen	">
         <nav className="hidden sm:flex flex-col w-[370px] min-h-[calc(100vh-3.0rem)] bg-slate-800	bg-opacity-30 p-5 border-r-2  border-r-slate-700">
           <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-indigo-700 via-indigo-200 bg-clip-text text-transparent text-center">
             BrainWave
